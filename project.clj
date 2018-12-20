56e5e473b1caa7401d0a9440d94f91259beed8a5
Chris Nuernberger <chris@thinktopic.com>
2018-12-20 15:13:08 -0700

    2.7

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject techascent/tech.ml-base "2.7-SNAPSHOT"
+(defproject techascent/tech.ml-base "2.7"
   :description "Base concepts of the techascent ml suite"
   :url "http://github.com/tech-ascent/tech.ml-base"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [techascent/tech.compute "3.0"]
                  [techascent/tech.io "2.1"]
                  [camel-snake-kebab "0.4.0"]
