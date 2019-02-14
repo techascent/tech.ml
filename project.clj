2657afa6b42d983405658e09a9bb7e84c38d06b9
Chris Nuernberger <chris@thinktopic.com>
2019-02-14 10:47:11 -0700

    Moved dataset to separate project.

--- project.clj
+++ project.clj
@@ -4,11 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [techascent/tech.compute "3.7"]
+                 [techascent/tech.ml.dataset "0.1"]
                  [techascent/tech.io "2.8"]
-                 [camel-snake-kebab "0.4.0"]
-                 [org.apache.commons/commons-math3 "3.6.1"]
-                 [tech.tablesaw/tablesaw-core "0.30.2"]
-                 [ml.dmlc/xgboost4j "0.81"]
-                 [com.github.haifengl/smile-core "1.5.2"]]
+                 [ml.dmlc/xgboost4j "0.81"]]
   :java-source-paths ["java"])
