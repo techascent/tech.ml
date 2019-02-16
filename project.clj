3773f3586a4a068bafa0a378617407d45d1cd9e8
Chris Nuernberger <chris@thinktopic.com>
2019-02-16 12:34:03 -0700

    0.9

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject techascent/tech.ml "0.9-SNAPSHOT"
+(defproject techascent/tech.ml "0.9"
   :description "Base concepts of the techascent ml suite"
   :url "http://github.com/techascent/tech.ml-base"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [techascent/tech.ml.dataset "0.5"]
+                 [techascent/tech.ml.dataset "0.6"]
                  [techascent/tech.io "2.8"]
                  [ml.dmlc/xgboost4j "0.81"]]
   :java-source-paths ["java"])
