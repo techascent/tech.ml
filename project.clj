c9a922d77352034237c58f94e98e01b85df563c1
Chris Nuernberger <chris@techascent.com>
2019-02-23 14:46:22 -0700

    0.16

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject techascent/tech.ml "0.16-SNAPSHOT"
+(defproject techascent/tech.ml "0.16"
   :description "Base concepts of the techascent ml suite"
   :url "http://github.com/techascent/tech.ml-base"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [techascent/tech.ml.dataset "0.14"]
+                 [techascent/tech.ml.dataset "0.15"]
                  [techascent/tech.io "2.8"]
                  [ml.dmlc/xgboost4j "0.81"]]
 
