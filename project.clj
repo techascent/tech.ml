8a0c6f16d47168a281439db5c131bb0bffd55423
Chris Nuernberger <chris@thinktopic.com>
2019-03-02 16:01:32 -0700

    Adding a clojure 1.10 pathway.

--- project.clj
+++ project.clj
@@ -3,12 +3,15 @@
   :url "http://github.com/techascent/tech.ml-base"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [techascent/tech.ml.dataset "0.20"]
+  :dependencies [[techascent/tech.ml.dataset "0.20"]
                  [techascent/tech.io "2.8"]
                  [ml.dmlc/xgboost4j "0.81"]]
 
-  :profiles {:dev {:dependencies [[org.clojure/tools.logging "0.3.1"]
-                                  [ch.qos.logback/logback-classic "1.1.3"]]}}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
+                                  [org.clojure/tools.logging "0.3.1"]
+                                  [ch.qos.logback/logback-classic "1.1.3"]]}
+             :clojure-10 {:dependencies [[org.clojure/clojure "1.10.0"]
+                                         [org.clojure/tools.logging "0.3.1"]
+                                         [ch.qos.logback/logback-classic "1.1.3"]]}}
 
   :java-source-paths ["java"])
