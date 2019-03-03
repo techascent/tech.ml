1493cdf0348fd1b61d4aea6b8b0afb7c988b452b
Chris Nuernberger <chris@thinktopic.com>
2019-03-03 10:13:34 -0700

    0.22

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject techascent/tech.ml "0.22-SNAPSHOT"
+(defproject techascent/tech.ml "0.22"
   :description "Base concepts of the techascent ml suite"
   :url "http://github.com/techascent/tech.ml-base"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[techascent/tech.ml.dataset "0.20"]
+  :dependencies [[techascent/tech.ml.dataset "0.21"]
                  [techascent/tech.io "2.8"]
                  [ml.dmlc/xgboost4j "0.81"]]
 
