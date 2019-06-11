48ed0de3ec5a958ecf4e54d18be58f62909e25ed
Chris Nuernberger <chris@techascent.com>
2019-06-11 15:04:44 -0600

    updating to deps.edn.

--- project.clj
+++ project.clj
@@ -3,10 +3,9 @@
   :url "http://github.com/techascent/tech.ml-base"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[techascent/tech.ml.dataset "1.0-alpha10"]
-                 [techascent/tech.io "2.11"]
-                 [ml.dmlc/xgboost4j "0.81"]]
-
+  :plugins [[lein-tools-deps "0.4.1"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1-beta2"]
                                   [ch.qos.logback/logback-classic "1.1.3"]
                                   [metasoarous/oz "1.5.2"]]}}
