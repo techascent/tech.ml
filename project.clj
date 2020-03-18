96d0bf959f82e6bc6e166ebb41bc0de019ff7323
Chris Nuernberger <chris@techascent.com>
2020-03-18 15:45:46 -0600

    2.0-beta-1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.ml "2.0-alpha-1-1-SNAPSHOT"
+(defproject techascent/tech.ml "2.0-beta-1"
   :description "Base concepts of the techascent ml suite"
   :url "http://github.com/techascent/tech.ml-base"
   :license {:name "Eclipse Public License"
