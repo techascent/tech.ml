d1c3405e623306c849a1d86a71049e07d9b9e492
Chris Nuernberger <chris@techascent.com>
2020-06-07 07:51:48 -0600

    2.0-beta-56-1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.ml "2.0-beta-57-SNAPSHOT"
+(defproject techascent/tech.ml "2.0-beta-56-1"
   :description "Base concepts of the techascent ml suite"
   :url "http://github.com/techascent/tech.ml-base"
   :license {:name "Eclipse Public License"
@@ -6,5 +6,4 @@
   :plugins [[lein-tools-deps "0.4.5"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
   :lein-tools-deps/config {:config-files [:install :user :project]}
-  :profiles {:dev {:lein-tools-deps/config {:resolve-aliases [:test]}}}
-  :java-source-paths ["java"])
+  :profiles {:dev {:lein-tools-deps/config {:resolve-aliases [:test]}}})
