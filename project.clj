961389a849829214d63a15efcbc9044090435179
Chris Nuernberger <chris@techascent.com>
2020-07-09 08:54:43 -0600

    Adding tech.ml.dataset as provided for docs.

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-tools-deps "0.4.5"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :dependencies [[techascent/tech.ml.dataset "3.01" :scope "provided"]]
   :lein-tools-deps/config {:config-files [:install :user :project]}
   :profiles {:dev {:lein-tools-deps/config {:resolve-aliases [:test]}}})
