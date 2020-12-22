0a573c6958671a3c35423e91dc6ebdc99d77ba87
Carsten Behring <carsten.behring@gmail.com>
2020-12-22 22:40:01 +0100

    refactored

--- src/tech/v3/libs/smile/discrete_nb.clj
+++ src/tech/v3/libs/smile/discrete_nb.clj
@@ -1,16 +1,10 @@
 (ns tech.v3.libs.smile.discrete-nb
-
-  (:require [pppmap.core :as ppp]
-            [tech.v3.dataset :as ds]
+  (:require [tech.v3.dataset :as ds]
             [tech.v3.dataset.modelling :as ds-mod]
             [tech.v3.libs.smile.nlp :as nlp]
-            [tech.v3.ml :as ml]
-            [tech.v3.libs.smile.maxent :as maxent]
-            )
+            [tech.v3.ml :as ml])
   (:import [smile.classification DiscreteNaiveBayes DiscreteNaiveBayes$Model]
-           [smile.util SparseArray])
-
-  )
+           smile.util.SparseArray))
 
 (defn freqs->SparseArray [freq-map vocab->index-map]
   (let [sparse-array (SparseArray.)]
@@ -19,61 +13,42 @@
        (when (contains? vocab->index-map token)
          (.append sparse-array ^int (get vocab->index-map token) ^double freq)))
      freq-map)
-    sparse-array
-    ))
+    sparse-array))
 
 (defn bow->SparseArray [ds bow-col indices-col vocab-size]
   "Converts a bag-of-word column `bow-col` to sparse indices column `indices-col`,
-   as needed by the Maxent model."
-  (let [vocabulary (nlp/->vocabulary-top-n ds bow-col vocab-size)
-        vocab->index-map (:vocab->index-map vocabulary)
-        ds
-        (vary-meta ds assoc
-                   :count-vectorize-vocabulary vocabulary)]
-    (ds/add-or-update-column
-     ds
-     (ds/new-column
-      indices-col
-      (ppp/ppmap-with-progress
-       "bow->SparseArray"
-       1000
-       #(freqs->SparseArray % vocab->index-map)
-       (get ds bow-col))))))
+   as needed by the discrete naive bayes model. `vocab size` is the size of vocabluary used, sorted by token frequency "
+  (nlp/bow->something-sparse ds bow-col indices-col vocab-size freqs->SparseArray))
 
 
 (defn train [feature-ds target-ds options]
-  "Training function of Maxent model
-   The first feature column of `feature-ds` needs to contain the text as a sparce vector
-   agains the vocabulary."
+  "Training function of discrete naive bayes model.
+   The column of name `(options :sparse-colum)` of `feature-ds` needs to contain the text as SparseArrays
+   over the vocabulary."
   (let [train-array (into-array SparseArray
                                 (get feature-ds (:sparse-column options)))
         train-score-array (into-array Integer/TYPE
                                       (get target-ds (first (ds-mod/inference-target-column-names target-ds))))
         p (count (-> feature-ds meta :count-vectorize-vocabulary :vocab->index-map))
-        ;; options (merge maxent-default-parameters options)
-        nb (DiscreteNaiveBayes. DiscreteNaiveBayes$Model/BERNOULLI 5 p)]
+        nb-model
+        (case (options :discrete-naive-bayes-model)
+          :bernoulli  DiscreteNaiveBayes$Model/BERNOULLI
+          :multinomial DiscreteNaiveBayes$Model/MULTINOMIAL)
+        nb (DiscreteNaiveBayes. nb-model (:k options) p)]
     (.update nb
              train-array
-             train-score-array
-
-             ;; (:lambda options)
-             ;; (:tol options)
-             ;; (:max-iter options)
-             )
-    nb
-    ))
+             train-score-array)
+    nb))
 
 (defn predict [feature-ds
                       thawed-model
                       model]
-  "Predict function for discrete naive bayses"
-  (let [predict-array
-        (into-array ^"[[Ljava.lang.Integer"
-                    (get feature-ds :bow-sparse))
-        target-colum (first (:target-columns model))]
-    (ds/->dataset (hash-map
-                   target-colum
-                   (seq  (.predict (:model-data model) predict-array))))))
+  "Predict function for discrete naive bayes"
+  (let [sparse-arrays (get feature-ds :bow-sparse)
+        target-colum (first (:target-columns model))
+        predictions (map #(.predict (:model-data model) %) sparse-arrays)
+        ]
+    (ds/->dataset {target-colum predictions})) )
 
 
 (ml/define-model!
---
+++ test/tech/v3/libs/smile/discrete_nb_test.clj
@@ -0,0 +1,59 @@
+(ns tech.v3.libs.smile.discrete-nb-test
+  (:require [clojure.test :refer :all]
+            [tech.v3.dataset :as ds]
+            [tech.v3.dataset.modelling :as ds-mod]
+            [tech.v3.libs.smile.discrete-nb :as nb]
+            [tech.v3.libs.smile.nlp :as nlp]
+            [tech.v3.ml :as ml]))
+
+(defn get-reviews []
+  (->
+   (ds/->dataset "test/data/reviews.csv.gz" {:key-fn keyword })
+   (ds/select-columns [:Text :Score])
+   (ds/update-column :Score #(map dec %))
+   (nlp/count-vectorize :Text :bow nlp/default-text->bow)
+   (nb/bow->SparseArray :bow :bow-sparse 100)
+   (ds-mod/set-inference-target :Score)))
+
+
+
+
+(deftest freqs->SparseArray []
+  (is (=
+       (map
+        #(vector (.i %) (.x %))
+        (iterator-seq
+         (.iterator (nb/freqs->SparseArray {"a" 10.0  "b" 70.0 "c" 50.0} {"a" 0 "b" 1} ))))
+       [[0 10.0] [1 70.0] ])))
+
+
+(deftest test-discrete-nb-bernoulli
+  (is (=
+       (:Score
+        (let [reviews (get-reviews)
+              trained-model
+              (ml/train reviews {:model-type :discrete-naive-bayes
+                                 :discrete-naive-bayes-model :bernoulli
+                                 :sparse-column :bow-sparse
+                                 :k 5
+                                 })
+              prediction (ml/predict (ds/head reviews 10) trained-model)]
+          prediction
+          ))
+       [3 3 3 3 3 4 4 3 3 3])))
+
+(deftest test-discrete-nb-multinomial
+  (is (=
+       (:Score
+        (let [reviews (get-reviews)
+              trained-model
+              (ml/train reviews {:model-type :discrete-naive-bayes
+                                 :discrete-naive-bayes-model :multinomial
+                                 :sparse-column :bow-sparse
+                                 :k 5
+                                 })
+              prediction (ml/predict (ds/head reviews 10) trained-model)
+              ]
+          prediction
+          ))
+       [4 4 3 2 3 4 4 4 4 4])))
