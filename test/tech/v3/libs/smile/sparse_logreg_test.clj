0a046f6a6749a1df4382f212b310357e23462861
Carsten Behring <carsten.behring@gmail.com>
2021-01-03 22:41:59 +0100

    added psrase svm and sparse logistic regresssion (#24)

---
+++ test/tech/v3/libs/smile/sparse_logreg_test.clj
@@ -0,0 +1,27 @@
+(ns tech.v3.libs.smile.sparse-logreg-test
+  (:require [clojure.test :refer :all]
+            [tech.v3.dataset :as ds]
+            [tech.v3.dataset.modelling :as ds-mod]
+            [tech.v3.libs.smile.discrete-nb :as nb]
+            [tech.v3.libs.smile.nlp :as nlp]
+            [tech.v3.ml :as ml]
+            [tech.v3.libs.smile.sparse-logreg]))
+
+(defn get-reviews []
+  (->
+   (ds/->dataset "test/data/reviews.csv.gz" {:key-fn keyword })
+   (ds/select-columns [:Text :Score])
+   (ds/update-column :Score #(map dec %))
+   (nlp/count-vectorize :Text :bow nlp/default-text->bow)
+   (nb/bow->SparseArray :bow :bow-sparse 100)
+   (ds-mod/set-inference-target :Score)))
+
+(deftest does-not-crash
+  (let [reviews (get-reviews)
+        trained-model
+        (ml/train reviews {:model-type :smile.classification/sparse-logistic-regression
+                          :sparse-column :bow-sparse})]
+
+    (is (= [4 4 4 2]
+           (take 4
+                 (:Score  (ml/predict reviews trained-model)))))))
