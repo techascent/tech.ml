5ecfba2152f6dcd2fadd37ab404926c3938e136d
Carsten Behring <carsten.behring@gmail.com>
2021-02-14 15:32:56 +0100

    made method more compact

--- src/tech/v3/ml/metamorph.clj
+++ src/tech/v3/ml/metamorph.clj
@@ -1,12 +1,8 @@
 (ns tech.v3.ml.metamorph
   (:require [tech.v3.ml :as ml]))
 
-
 (defn model [options]
-  (fn [ctx]
-    (let [id (:metamorph/id ctx)
-          ds (:metamorph/data ctx)
-          mode (:metamorph/mode ctx)]
-      (case mode
-        :fit (assoc ctx id (ml/train ds  options))
-        :transform  (assoc ctx :metamorph/data (ml/predict ds (get ctx id)))))))
+  (fn [{:metamorph/keys [id data mode] :as ctx}]
+    (case mode
+      :fit (assoc ctx id (ml/train data  options))
+      :transform  (assoc ctx :metamorph/data (ml/predict data (get ctx id))))))
