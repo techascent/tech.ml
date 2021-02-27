657cfbe27d107c905c36f7fcc65a3c19c59001e7
Chris Nuernberger <chris@techascent.com>
2021-02-27 09:57:35 -0700

    snap

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.ml "5.01"
+(defproject techascent/tech.ml "5.02-SNAPSHOT"
   :description "Basic machine learning toolkit.  `tech.v3.ml` is the root
 namespace and provides train/predict pathways based on datasets and
 an options map.  Please see the xgboost article for a quick runthough
